45dd21513d125d5cf6f06a1e1c95f1ac850a2713
linkcoder <718681050@qq.com>
2019-03-05 15:19:02 +0800

    init design code

---
+++ design-patterns/design-code/src/main/java/cn/link/designcode/DesignCodeApplication.java
@@ -0,0 +1,13 @@
+package cn.link.designcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignCodeApplication.class, args);
+    }
+
+}
