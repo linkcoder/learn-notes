a09b958ab2fbfacfeaf16b7b09f378042bf04c79
lincoder <718681050@qq.com>
2019-04-12 21:35:56 +0800

    电话号码的字母组合

---
+++ algorithm/ac-leetcode/src/cn/link/acleetcode/c17/Solution.java
@@ -0,0 +1,44 @@
+package cn.link.acleetcode.c17;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    static String[] keys={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+    static StringBuilder str = new StringBuilder();
+
+
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+
+        if(digits == null || digits.equals("") || digits.length() == 0) return result;
+
+        buildString(0,digits,result);
+        return result;
+    }
+
+    private void buildString(int index, String digits, List<String> result){
+        if(index == digits.length()){
+            result.add(str.toString());
+            return;
+        }
+
+        int keyIndex = digits.charAt(index) - '0';
+        String key = keys[keyIndex];
+        for(int i=0;i< key.length(); i++){
+            char c = key.charAt(i);
+            str.append(c);
+            buildString(index+1, digits, result);
+            str.deleteCharAt(str.length()-1);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        List<String> strings = solution.letterCombinations("");
+        strings.forEach(System.out::println);
+
+    }
+}
