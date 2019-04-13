88cfbba431f322b5bb00103186f8c541757c694f
linkcoder <718681050@qq.com>
2019-04-13 16:16:43 +0800

    druid 问题 待解

---
+++ frame/druid/src/main/java/cn/link/frame/druid/Connect.java
@@ -0,0 +1,49 @@
+package cn.link.frame.druid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+@Component
+@Order(1)
+public class Connect implements CommandLineRunner {
+
+//    @Autowired
+//    private DataSource dataSource;
+
+    @Autowired
+    @Qualifier("one")
+    private DataSource one;
+
+    @Autowired
+    @Qualifier("two")
+    private DataSource two;
+
+    @Override
+    public void run(String... args) throws Exception {
+//        readData(dataSource.getConnection());
+        readData(one.getConnection());
+        readData(two.getConnection());
+    }
+
+    public void readData(Connection connect) throws SQLException {
+        Statement st = connect.createStatement();
+        ResultSet rs = st.executeQuery("select * from apps");
+        while(rs.next()){
+            System.out.print(rs.getString(1) + " ");
+            System.out.print(rs.getString(2) + " ");
+            System.out.print(rs.getString(3) + " ");
+            System.out.print(rs.getString(4) + " ");
+            System.out.println();
+        }
+    }
+}
