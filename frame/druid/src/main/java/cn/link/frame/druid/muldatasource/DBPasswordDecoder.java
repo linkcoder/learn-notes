5ebf66aee5bf5b3e0f2ae4d302f912cf6e168d00
lincoder <718681050@qq.com>
2019-04-13 23:39:42 +0800

    自定义druid解码器

---
+++ frame/druid/src/main/java/cn/link/frame/druid/muldatasource/DBPasswordDecoder.java
@@ -0,0 +1,35 @@
+package cn.link.frame.druid.muldatasource;
+
+import com.alibaba.druid.filter.config.ConfigTools;
+import com.alibaba.druid.util.DruidPasswordCallback;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class DBPasswordDecoder extends DruidPasswordCallback {
+
+    private Logger logger = LoggerFactory.getLogger(DBPasswordDecoder.class);
+
+    private String publicKey;
+    private String ciphertext;
+
+    public DBPasswordDecoder(String publicKey, String ciphertext) {
+        super();
+        this.publicKey = publicKey;
+        this.ciphertext = ciphertext;
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+        super.setProperties(properties);
+        try {
+            String password = ConfigTools.decrypt(publicKey, ciphertext);
+            logger.info("decode success and password is {}", password);
+            setPassword(password.toCharArray());
+        } catch (Exception e) {
+            logger.warn("try decode fail");
+        }
+    }
+
+}
--- frame/druid/src/main/java/cn/link/frame/druid/muldatasource/DatasourceConfig.java
+++ frame/druid/src/main/java/cn/link/frame/druid/muldatasource/DatasourceConfig.java
@@ -1,13 +1,10 @@
 package cn.link.frame.druid.muldatasource;
 
-import com.alibaba.druid.filter.config.ConfigTools;
 import com.alibaba.druid.pool.DruidDataSource;
-import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 
@@ -17,27 +14,25 @@ public class DatasourceConfig {
 
     @Bean(name = "one")
     @ConfigurationProperties("spring.datasource.druid.one")
-    public DataSource dataSource1(@Value("${spring.datasource.druid.one.password}") String ciphertext ) {
-        System.out.println(ciphertext);
+    public DataSource dataSource1(@Value("${publickey}") String publicKey,
+                                  @Value("${spring.datasource.druid.one.password}") String ciphertext) {
+
+        DBPasswordDecoder decoder = new DBPasswordDecoder(publicKey, ciphertext);
+
         DruidDataSource ds = new DruidDataSource();
-        ds.setPassword(ciphertext);
+        ds.setPasswordCallback(decoder);
         return ds;
     }
 
     @Bean(name = "two")
     @ConfigurationProperties("spring.datasource.druid.two")
-    public DataSource dataSource2(
-//            @Value("${spring.datasource.druid.two.password}") String ciphertext
-    ){
-//        System.out.println(ciphertext);
-        DruidDataSource ds = null;
-        try {
-            ds = new MyDataSource();
-            ds.setPassword("hellosql");
-        } catch (Exception e) {
-            System.out.println(ds.getPassword());
-            e.printStackTrace();
-        }
+    public DataSource dataSource2(@Value("${publickey}") String publicKey,
+                                  @Value("${spring.datasource.druid.two.password}") String ciphertext){
+
+        DBPasswordDecoder decoder = new DBPasswordDecoder(publicKey, ciphertext);
+
+        DruidDataSource ds = new DruidDataSource();
+        ds.setPasswordCallback(decoder);
         return ds;
     }
 }
