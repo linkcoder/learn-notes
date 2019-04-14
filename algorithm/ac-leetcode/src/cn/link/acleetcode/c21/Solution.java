30608622b4956314316b37841b85a385a5aabbff
lincoder <718681050@qq.com>
2019-04-14 12:14:38 +0800

    合并两个链表

--- algorithm/ac-leetcode/src/cn/link/acleetcode/c21/Solution.java
+++ algorithm/ac-leetcode/src/cn/link/acleetcode/c21/Solution.java
@@ -44,4 +44,21 @@ public class Solution {
         }
         return head.next;
     }
+
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+
+        if(l1.val < l2.val){
+            l1.next = mergeTwoLists2(l1.next, l2);
+            return l1;
+        }else{
+            l2.next = mergeTwoLists2(l2.next, l1);
+            return l2;
+        }
+    }
+
+
 }
+
---
+++ algorithm/ac-leetcode/src/cn/link/acleetcode/c23/Solution.java
@@ -0,0 +1,12 @@
+package c23;
+
+/**
+ * 题目：合并K个排序链表
+ * 描述：合并 k 个排序链表，返回合并后的排序链表。
+ */
+public class Solution {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+
+    }
+}
