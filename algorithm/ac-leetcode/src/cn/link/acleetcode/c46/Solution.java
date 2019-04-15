93fc7e857a76a4be5b8584bda65af8b582fe261e
lincoder <718681050@qq.com>
2019-04-15 20:47:14 +0800

    组合数

--- algorithm/ac-leetcode/src/cn/link/acleetcode/c46/Solution.java
+++ algorithm/ac-leetcode/src/cn/link/acleetcode/c46/Solution.java
@@ -1,4 +1,58 @@
 package cn.link.acleetcode.c46;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 题目：组合数
+ * 描述：给定一个没有重复数字的序列，返回其所有可能的全排列
+ */
 public class Solution {
+
+    List<Integer> str = new ArrayList<>();
+
+    public List<List<Integer>> permute(int[] nums) {
+
+        if(nums == null || nums.length == 0) return null;
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        if(nums.length == 1) {
+            List item = new ArrayList<>();
+            item.add(nums[0]);
+            result.add(item);
+            return result;
+        }
+
+        huishuo(nums, result, 0);
+
+        return result;
+    }
+
+    public void huishuo(int[] nums, List<List<Integer>> result, int index){
+
+        if(index == nums.length){
+            List item = new ArrayList<>();
+            item.addAll(str);
+            result.add(item);
+            return;
+        }
+        for(int i=0; i<nums.length; i++){
+            if(str.contains(nums[i])) continue;
+            str.add(nums[i]);
+            huishuo(nums, result, index+1);
+            str.remove(str.size()-1);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = new int[]{1,2,3};
+        System.out.println(solution.permute(nums));
+
+        nums = new int[]{1};
+        System.out.println(solution.permute(nums));
+    }
 }
---
+++ algorithm/ac-leetcode/src/cn/link/acleetcode/c53/Solution.java
@@ -0,0 +1,45 @@
+package cn.link.acleetcode.c53;
+
+/**
+ * 题目：最大子序和
+ * 描述：给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和
+ */
+public class Solution {
+
+    /**
+     * 当我们加上一个正数时，和会增加；当我们加上一个负数时，和会减少。
+     * 如果当前得到的和是个负数，那么这个和在接下来的累加中应该抛弃并重新清零，不然的话这个负数将会减少接下来的和。
+     * @param nums
+     * @return
+     */
+    public int maxSubArray(int[] nums) {
+
+        if(nums == null || nums.length == 0) return 0;
+
+        int current = nums[0];
+        int sum = nums[0];
+
+        for(int i=1; i<nums.length; i++){
+
+            if(current<0){
+                current = nums[i];
+            } else{
+                current += nums[i];
+            }
+
+            if(current > sum) sum = current;
+        }
+        return sum;
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        int result = solution.maxSubArray(nums);
+        System.out.println(result);
+    }
+}
